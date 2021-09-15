a9d101e39374cb2226c486be63e1becc32b40d88
Sasha <lDonKabanl@yandex.ru>
2021-09-15 16:20:30 +0300

    fixed

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function getInfo () {
             const xmlContent = xml;
 
             const parser = new DOMParser(),
-                xmlDom = parser.parseFromString(xmlContent, 'application/xml'),
-                characters = xmlDom.querySelectorAll('character'),
-                comics = xmlDom.querySelectorAll('comic');
+                  xmlDom = parser.parseFromString(xmlContent, 'application/xml'),
+                  characters = xmlDom.querySelectorAll('character'),
+                  comics = xmlDom.querySelectorAll('comic');
 
             let arr = [];
 
@@ -40,7 +40,7 @@ function getInfo () {
 
                 comics.forEach(comic => {
                     const heroes = comic.getAttribute('heroes'),
-                        title = comic.getAttribute('name');
+                          title = comic.getAttribute('name');
                     
                     
                     if (heroes.match(hero.getAttribute('name'))) {
@@ -63,18 +63,21 @@ function getInfo () {
             arr.sort((a, b) => {
             return (a.sumLevels < b.sumLevels) - (b.sumLevels < a.sumLevels) || (a.mainSkill < b.mainSkill) - (b.mainSkill < a.mainSkill);
             });
+            heroPlaces(arr);
             showInfo(arr);
         });
 }
     
-function showInfo(arr) {
+function heroPlaces(arr) {
     const container = document.createElement('div'),
           listHeroes = document.createElement('ol');
+    listHeroes.classList.add('list');
     
     arr.forEach(hero => {
         const place = document.createElement('li'),
               img = document.createElement('img');
         
+        place.classList.add('list__item');
         img.src = `images/${hero.name}.png`;
         place.append(img);
         listHeroes.append(place);
@@ -84,4 +87,33 @@ function showInfo(arr) {
     container.append(listHeroes);
 }
 
+function showInfo (arr) {
+    const images = document.querySelectorAll('img'),
+          info = document.createElement('div');
+
+    info.classList.add('info');
+
+    images.forEach(img => {
+        img.addEventListener('click', () => {
+            arr.forEach(hero => {
+                if (img.getAttribute('src').match(hero.name)) {
+                    info.innerHTML = `
+                    <p>
+                        Имя: ${hero.name} <br>
+                        Количество умений: ${hero.numberOfSkills} <br>
+                        Сумма уровней всех навыков: ${hero.sumLevels} <br>
+                        Ключевой навык: ${hero.mainSkillName} <br>
+                        Количество комиксов с героем: ${hero.numberOfComics} <br>
+                        Названия комиксов: ${hero.titleOfComics} 
+                    </p>
+                    `;
+                }
+            });
+        });
+        document.body.append(info);
+    });
+}
+
 getInfo();
+
+
